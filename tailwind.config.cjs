b98abf9d784b3be92551e6c4f5c77017533b396e
pantheon-decoupled-service-user <decoupled-service-user@pantheon.io>
2023-12-14 21:47:56 +0000

    Merge pull request #1006 from pantheon-systems/changeset-release/main

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,9 +4,7 @@ const { tailwindcssPlugin } = require('@pantheon-systems/wordpress-kit');
 /** @type {import('@pantheon-systems/wordpress-kit').TailwindcssConfig} */
 module.exports = {
 	content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
-		'./src/**/*.{js,jsx,ts,tsx}'
+		'./src/{pages,components,templates}/**/*.{jsx,tsx}'
 	],
 	theme: {
 		extend: {},
